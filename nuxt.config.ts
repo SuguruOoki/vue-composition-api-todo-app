90b57da547bb6822f2a2a561617ac5b69c0ce1d6
SuguruOoki <oki.work0527@gmail.com>
2020-11-14 23:03:33 +0900

    Fix nuxt.config name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,8 +17,9 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [],
-
+  plugins: [
+    '~/plugins/CompositionApi'
+  ],
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
