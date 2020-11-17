6f771bda7ef869c7664689219531081fefc50c19
SuguruOoki <oki.work0527@gmail.com>
2020-11-17 22:22:22 +0900

    Fix index

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,79 @@
+/* eslint-disable vue/max-attributes-per-line */
 <template>
   <div class="container">
-    <div>
-      <Logo />
+    <div class="title">
+      <h1>TODO APP</h1>
+    </div>
+    <form class="add">
+      <label id="title">title</label>
+      <input v-model="title" for="title" type="text" />
+      <label id="description">description</label>
+      <input v-model="description" for="description" type="text" />
+      <button
+        type="button"
+        @click="add"
+      >
+        追加
+      </button>
+    </form>
+    <div
+      v-for="item in items" :key="item.id"
+      class="list-container"
+      >
+      <div class="item-container">
+        <div class="item-title">
+          {{ item.title }}
+        </div>
+        <div class="item-description">
+          {{ item.description }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
-<style>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      title: '' as string,
+      description: '' as string,
+      items: [
+        {
+          title: 'todo',
+          description: 'description1',
+        },
+        {
+          title: 'todo2',
+          description: 'description2',
+        },
+        {
+          title: 'todo3',
+          description: 'description3',
+        },
+      ],
+    }
+  },
+  methods: {
+    add() {
+      const item = {
+        title: this.title,
+        description: this.description,
+      }
+      console.log
+      this.items.push(item)
+    },
+  },
+})
+</script>
+<style lang="scss">
 .container {
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+
+  .list-container {
+    background-color: #35495e;
+  }
 }
 
 .title {
@@ -20,7 +81,7 @@
     'Helvetica Neue', Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 28px;
   color: #35495e;
   letter-spacing: 1px;
 }
@@ -31,6 +92,17 @@
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
+}
+
+.item-container {
+  .item-title {
+    font-weight: 600;
+    font-size: 20px;
+  }
+
+  .item-description {
+    font-size: 16px;
+  }
 }
 
 .links {
